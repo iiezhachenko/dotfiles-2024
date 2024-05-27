5e92415032c717c72d60a0453352afc35386a010
iievgen <ievgen.iezhachenko@gmail.com>
2024-05-27 20:34:59 +0200

    Finished basic setup

--- .zshrc
+++ .zshrc
@@ -85,6 +85,7 @@ fi
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 alias vim="nvim"
+alias venv="source ./.venv/bin/activate"
 
 # Enable brew
 architecture=$(uname -m)
--- nvchad_custom/configs/null-ls.lua
+++ nvchad_custom/configs/null-ls.lua
@@ -1,10 +1,25 @@
-local none_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require("null-ls")
 
 local opts = {
   sources = {
-    none_ls.builtins.diagnostics.mypy,
-    -- none_ls.builtins.diagnostics.pylint,
-
-  }
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.mypy,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
 return opts
