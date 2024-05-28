0b777f426c5613d5937b5c950401ef135677152b
iievgen <ievgen.iezhachenko@gmail.com>
2024-05-28 09:20:44 +0200

    Disabled mypy for now

--- nvchad_custom/configs/null-ls.lua
+++ nvchad_custom/configs/null-ls.lua
@@ -4,7 +4,7 @@ local null_ls = require("null-ls")
 local opts = {
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.mypy,
+    -- null_ls.builtins.diagnostics.mypy,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
