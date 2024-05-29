ca4db5b416a9107f385acf3ceb55ac3085a610e0
iievgen <ievgen.iezhachenko@gmail.com>
2024-05-29 22:02:30 +0200

    Added Nvim Obsidian plugin

--- nvchad_custom/plugins.lua
+++ nvchad_custom/plugins.lua
@@ -64,5 +64,41 @@ local plugins = {
       require("core.utils").load_mappings("dap_python")
     end,
   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      "BufReadPre " .. vim.fn.expand "~" .. "/repos/Main Vault/**.md",
+      "BufNewFile " .. vim.fn.expand "~" .. "/repos/Main Vault/**.md",
+    },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter"
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/repos/Main Vault/",
+        },
+      },
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+    },
+},
 }
 return plugins
