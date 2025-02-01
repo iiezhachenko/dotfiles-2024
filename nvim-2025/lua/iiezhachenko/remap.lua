8d194da7ee6d5f57509b07764987669880b5c035
iievgen <ievgen.iezhachenko@gmail.com>
2025-02-01 23:24:30 +0100

    slowing down inefficient motions

--- nvim-2025/lua/iiezhachenko/remap.lua
+++ nvim-2025/lua/iiezhachenko/remap.lua
@@ -11,12 +11,18 @@ vim.keymap.set('n', '<C-q>', ':q<CR>', {
   desc = "Quit",
   noremap = true,
 })
-vim.keymap.set('n', ';', ':', {
-  desc = "Open command line",
-  noremap = true,
-})
+-- vim.keymap.set('n', ';', ':', {
+--   desc = "Open command line",
+--   noremap = true,
+-- })
 -- Insert mode navigation
 vim.keymap.set('i', '<C-h>', '<Left>')
 vim.keymap.set('i', '<C-l>', '<Right>')
 vim.keymap.set('i', '<C-j>', '<Down>')
 vim.keymap.set('i', '<C-k>', '<Up>')
+-- Adding delays to inefficient vim motions that I tend to abuse
+vim.keymap.set('n', 'j', "j:lua vim.cmd('sleep 300m')<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', 'k', "k:lua vim.cmd('sleep 300m')<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', 'e', "e:lua vim.cmd('sleep 300m')<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', 'w', "w:lua vim.cmd('sleep 300m')<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', 'b', "b:lua vim.cmd('sleep 300m')<CR>", { noremap = true, silent = true })
