11489332527537e5c9bba0816292a531d6fa4847
iievgen <ievgen.iezhachenko@gmail.com>
2025-02-02 16:32:31 +0100

    NVIM Obsidian path fix, increased delay on mappings

--- nvim-2025/lua/iiezhachenko/lazy_plugins/obsidian.lua
+++ nvim-2025/lua/iiezhachenko/lazy_plugins/obsidian.lua
@@ -6,8 +6,8 @@ return {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
     -- refer to `:h file-pattern` for more examples
-    "BufReadPre " .. vim.fn.expand "~" .. "/repos/Main Obsidian Vault/*.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/repos/Main Obsidian Vault/*.md"
+    "BufReadPre " .. vim.fn.expand "~" .. "/repos/Main Obsidian Vault/**/*.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/repos/Main Obsidian Vault/**/*.md"
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
